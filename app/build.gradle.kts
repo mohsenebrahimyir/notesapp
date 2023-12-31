7066a332f5f5d83485604e345a9410e317fb0a22
mohsenebrahimyir <mohsenebrahimy.ir@mail.com>
2024-01-01 00:59:19 +0330

    add Room and lifecycle libraries

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
     id("maven-publish")
+    id("kotlin-kapt")
 }
 
 android {
@@ -51,4 +52,15 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation("com.github.samanzamani:PersianDate:1.7.1")
+
+    //add Room
+    val roomVersion = "2.6.1"
+    implementation("androidx.room:room-ktx:$roomVersion")
+    implementation( "androidx.room:room-runtime:$roomVersion")
+    implementation("androidx.room:room-rxjava3:$roomVersion")
+    kapt("androidx.room:room-compiler:$roomVersion")
+
+    //Live Data
+    val lifecycleVersion = "2.6.1"
+    implementation ("androidx.lifecycle:lifecycle-livedata-ktx:$lifecycleVersion")
 }
